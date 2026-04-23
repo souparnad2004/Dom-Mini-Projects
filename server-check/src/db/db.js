def0878d6b07e2c06e07a2ac9035f41f5f679f3c
Souparna Dhara <189111059+souparnad2004@users.noreply.github.com>
2026-04-23 18:34:26 +0530

    Variable name change

--- server-check/src/db/db.js
+++ server-check/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected at: ", conn.connection.host);
     } catch (error) {
         console.log("DB connection error:", error.message);
